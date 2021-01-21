453f6aa937bdcbf4b37ec391651375dc8a425a53
wezley3 <some@mail.com>
2021-01-21 15:01:33 -0700

    Attempt to fix broken database initialization

--- full_install.sh
+++ full_install.sh
@@ -3,7 +3,7 @@
 # Wezley 3
 
 # Install python packages
-apt-get install -y python3
+apt-get install -y python3 sqlite3
 python3 -m pip install -U pip
 python3 -m pip install requests speedtest-cli
 
@@ -18,13 +18,17 @@ systemctl restart apache2
 
 # Create database for php
 mkdir -p /srv/databases/sst
+sqlite3 /srv/databases/sst/results.db "create table host(id int, url text);"
 chown -R www-data. /srv/databases/sst
-touch /srv/databases/sst/results.db
 
+# Allow other users added to www-data group to edit db
 chmod 776 /srv/databases/sst
 chmod 664 /srv/databases/sst/results.db
 
-cp ./html /var/www/html/simple_speed_test
+# Copy over html to apache
+cp -r ./html /var/www/html/simple_speed_test
 
+# Print out data for user
+echo "\n\nInstall Finished"
 echo "Data can now be viewed at http://127.0.0.1/simple_speed_test"
 echo "Data can be uploaded to http://127.0.0.1/simple_speed_test/php/post_results.php"
